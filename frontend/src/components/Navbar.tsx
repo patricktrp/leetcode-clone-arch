4d5167b21e0ae4528c15db5fe2a55cbb2d1a01d4
Patrick <patrick.treppmann97@gmail.com>
2023-06-20 21:30:57 +0200

    ui work

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import styles from './Navbar.module.css'
 import { useAuth0 } from '@auth0/auth0-react'
 import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs'
 import { SiLeetcode } from 'react-icons/si'
+import { Link } from 'react-router-dom'
+import { MdOutlineLogout } from 'react-icons/md'
 
 export type NavbarProps = {
     isDarkMode: boolean,
@@ -14,14 +16,18 @@ const Navbar = ({ toggleDarkMode, isDarkMode }: NavbarProps): JSX.Element => {
     return (
         <nav className={styles['navbar-layout']}>
             <div className={styles['navbar-inner-layout']}>
-                <SiLeetcode size="1.8em" />
+                <Link className={styles['navbar-icon']} to="/"><SiLeetcode size="1.8em" /></Link>
+
+                <ul className={styles['navigation-list']}>
+                    <li><Link className={styles['navigation-list-link']} to="problems">Problems</Link></li>
+                </ul>
 
                 <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                     {isDarkMode ? <BsFillSunFill onClick={toggleDarkMode} size="1.4em" /> : <BsFillMoonFill onClick={toggleDarkMode} size="1.4em" />}
                     {isAuthenticated ?
                         <div style={{ display: 'flex', alignItems: 'center' }}>
                             <img src={user?.picture} alt="" className={styles['user-picture']} />
-                            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout</button>
+                            <MdOutlineLogout className={styles['logout-button']} size="1.8em" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} />
                         </div> :
                         <div>
                             <button onClick={() => loginWithRedirect()}>Login</button>
--- frontend/src/routes/ProblemOverview.tsx
+++ frontend/src/routes/ProblemOverview.tsx
@@ -17,7 +17,7 @@ const ProblemOverview = (): JSX.Element => {
     return (
         <div>
             {MOCK_PROBLEMS.map(problem => {
-                return <Link to={problem.id}>{problem.id}</Link>
+                return <div><Link to={problem.id}>{problem.id}</Link></div>
             })}
         </div>
     )
