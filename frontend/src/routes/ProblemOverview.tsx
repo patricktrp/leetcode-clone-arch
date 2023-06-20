759b373706d989b87d6e187ce5a53653663e04e5
Patrick <patrick.treppmann97@gmail.com>
2023-06-20 23:38:36 +0200

    added @tanstack/react-table library for question overview page

--- frontend/src/routes/ProblemOverview.tsx
+++ frontend/src/routes/ProblemOverview.tsx
@@ -1,14 +1,84 @@
 import { Link } from 'react-router-dom'
 import mockData from '../assets/MOCK_DATA.json'
 import { useMemo } from 'react'
+import { useReactTable, getCoreRowModel, flexRender, createColumnHelper } from '@tanstack/react-table';
+
+type Problem = {
+    id: string,
+    difficulty: string,
+    categories: string[]
+}
 
 const ProblemOverview = (): JSX.Element => {
     const data = useMemo(() => mockData, [])
+    const columnHelper = createColumnHelper<Problem>()
+    const columns = [
+        columnHelper.accessor('id', {
+            header: 'Problem',
+            cell: x => <Link to={x.row.original.id}>{JSON.stringify(x.row.original.id)}</Link>
+        }),
+        columnHelper.accessor('difficulty', {
+            header: 'Difficulty'
+        }),
+        columnHelper.accessor('categories', {
+            header: 'Categories'
+        })
+        // columnHelper.accessor('age', {
+        //     header: () => 'Age',
+        //     cell: info => info.renderValue(),
+        //     footer: info => info.column.id,
+        // }),
+        // columnHelper.accessor('visits', {
+        //     header: () => <span>Visits</span>,
+        //     footer: info => info.column.id,
+        // }),
+        // columnHelper.accessor('status', {
+        //     header: 'Status',
+        //     footer: info => info.column.id,
+        // }),
+        // columnHelper.accessor('progress', {
+        //     header: 'Profile Progress',
+        //     footer: info => info.column.id,
+        // }),
+    ]
+
+    const table = useReactTable({
+        columns,
+        data,
+        getCoreRowModel: getCoreRowModel()
+    });
+
     return (
         <div>
-            {data.map(problem => {
-                return <div><Link to={problem.id}>{problem.id}</Link></div>
-            })}
+            <table>
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
